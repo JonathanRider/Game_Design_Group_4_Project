e051a8f03c325427b4eda7170dbb760d3a456619
syllospri <slith93@gmail.com>
2015-10-04 22:39:34 -0400

    Messed up the merge, fixed it though

--- bin/helloworld.cpp
+++ bin/helloworld.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
   InputSystem* inputS = new InputSystem(entityM, MENU);
 
 
-  GraphicsComponent* gc = new GraphicsComponent(shape);
+  GraphicsComponent* gc = new GraphicsComponent(sprite);
   ControllableComponent* cc = new ControllableComponent();
 
 
